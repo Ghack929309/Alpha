201eda69ebd8ac76f8694be76278a394c7064b73
Ghack929309 <ghack929309@gmail.com>
2021-10-11 18:55:35 +0300

    last update

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -8,8 +8,7 @@ const Context = ({ children }) => {
 	const [cartItems, setCartItems] = useState([]);
 	const [likeItems, setLikeItems] = useState([]);
 
-	const url =
-		"https://api.unsplash.com/search/photos?query=office&client_id=U2FpiuL0JDWmByRIeERJbq5_y2o8r6Sa1XN7rfKMwTQ";
+	const url = `https://api.unsplash.com/search/photos?query=office&client_id=U2FpiuL0JDWmByRIeERJbq5_y2o8r6Sa1XN7rfKMwTQ`;
 	// fetching data from unsplash api
 
 	useEffect(() => {
